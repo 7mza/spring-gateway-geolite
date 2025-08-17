f0df47eecfdcf6eafc43a5aaeeef34234843662b
Hamza BAAZIZ <9018321+7mza@users.noreply.github.com>
2025-08-17 14:42:48 +0200

    0.0.2-RELEASE

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val artifact = "spring-gateway-geolite"
 group = "io.github.7mza"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.2"
 
 val blockhoundVersion = "1.0.13.RELEASE"
 val geoip2Version = "4.3.1"
